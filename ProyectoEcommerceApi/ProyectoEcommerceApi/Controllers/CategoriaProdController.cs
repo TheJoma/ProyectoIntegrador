ada9a6f483511639c64f8f8178928c57cd647a2c
roo97 <ruvc9719@gmail.com>
2018-11-16 02:52:57 -0500

    Categoria Controller agregado

---
+++ ProyectoEcommerceApi/ProyectoEcommerceApi/Controllers/CategoriaProdController.cs
@@ -0,0 +1,25 @@
+﻿using ProyectoEcommerceModelos;
+using ProyectoEcommerceNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ProyectoEcommerceApi.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class CategoriaProdController : ApiController
+    {
+        CategoriaProdNegocio negocios = new CategoriaProdNegocio();
+
+        [HttpGet]
+        public List<CategoriaProducto> listarCategoriasProd()
+        {
+            var lista = negocios.listarCategoriasProd();
+            return lista;
+        }
+    }
+}
