6494cc62faae54501875abb8a299916b1b4c38d1
roo97 <ruvc9719@gmail.com>
2018-11-16 02:58:13 -0500

    Tipo usuario controller agregado

---
+++ ProyectoEcommerceApi/ProyectoEcommerceApi/Controllers/TipoUsuarioController.cs
@@ -0,0 +1,26 @@
+﻿using ProyectoEcommerceModelos;
+using ProyectoEcommerceNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ProyectoEcommerceApi.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class TipoUsuarioController : ApiController
+    {
+        TipoUsuarioNegocio negocios = new TipoUsuarioNegocio();
+        [HttpGet]
+        public List<TipoUsuario> listarTipoUsuario()
+        {
+            var lista = negocios.listarTipoUsuario();
+            return lista;
+        }
+    }
+}
+    
+
