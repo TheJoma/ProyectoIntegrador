6e68b7b0520fe86e495c94e435a7cc8c24390552
roo97 <ruvc9719@gmail.com>
2018-11-22 00:36:44 -0500

    Correccion modelo Producto

--- ProyectoEcommerceModelos/Producto.cs
+++ ProyectoEcommerceModelos/Producto.cs
@@ -8,21 +8,20 @@ namespace ProyectoEcommerceModelos
 {
     public class Producto
     {
-        public string codPro { get; set; }
+        public int codPro { get; set; }
         public string descripcionPro { get; set; }
         public string detallePro { get; set; }
         public double precioPro { get; set; }
         public int stockPro { get; set; }
+        public string imgPro { get; set; }
         public int codProdCat { get; set; }
         public int codProdMar { get; set; }
 
         public void validar()
         {
-            if(string.IsNullOrEmpty(codPro))
-                throw new Exception("Ingrese un codigo a su productos");
-            if (codPro.Length != 6)
-                throw new Exception("Debe contener 6 caractes el codigo de producto");
-            if (string.IsNullOrEmpty(codPro))
+         
+         
+            if (string.IsNullOrEmpty(codPro.ToString()))
                 throw new Exception("Ingrese un codigo a su productos");
             if (string.IsNullOrEmpty(descripcionPro))
                 throw new Exception("Debe colocar descripcion a su  producto");
@@ -36,6 +35,8 @@ namespace ProyectoEcommerceModelos
                 throw new Exception("Debe seleccionar una categoria");
             if (string.IsNullOrEmpty(codProdMar.ToString()))
                 throw new Exception("Debe seleccionar una marca");
+            if (string.IsNullOrEmpty(imgPro.ToString()))
+                throw new Exception("Ingrese un codigo a su productos");
 
 
 
