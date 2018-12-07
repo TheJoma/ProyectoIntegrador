ac1cb959714286e59995d6c6fcd18f317c7213c2
roo97 <ruvc9719@gmail.com>
2018-12-06 22:29:06 -0500

    base de datos arreglada

--- BD_ECOMMERCE_GRUPO2_DICIEMBRE-FIX.sql
+++ BD_ECOMMERCE_GRUPO2_DICIEMBRE-FIX.sql
@@ -9,6 +9,7 @@ End
 go
 
 
+
 Create database BD_PROYECTO_INTEGRADOR
 go
 
@@ -28,10 +29,12 @@ go*/
 codTipoUsu int identity(1,1) primary key,
 desTipoUsu varchar(25) not null)
 go
+
 insert into tb_tipo_usuario values('ADMINISTRADOR')
 insert into tb_tipo_usuario values('EMPLEADO')
 insert into tb_tipo_usuario values('CLIENTE')
 go
+
 select * from tb_tipo_usuario
 go*/
 
@@ -41,7 +44,7 @@ codUsu	int identity(1,1) primary key ,
 nomUsu	varchar(25) not null,
 apePatUsu varchar(25) not null,
 apeMatUsu varchar(25) not null,
-dniUsu char(8) not null,
+dniUsu char(8) not null unique,
 celUsu char(9) not null,
 nickUsu	varchar(25) not null unique,
 pwdUsu	varchar(25) not null,
@@ -51,10 +54,14 @@ mailUsu	varchar(50) not null
 )
 go
 
+
+
 insert into tb_usuario values('JOSE MARIA','PARIASCA','MATEO',12345678,123456789,'JOMA','josemaria',getdate(),'joma@hotmail.com')
 go
 insert into tb_usuario values('RODRIGO','URDAY','VARGAS-CORBACHO',87654321,936692228,'ADM','ro',getdate(),'joma@hotmail.com')
 go
+
+
 
 
 --Creacion tabla categorias del producto
@@ -92,7 +99,7 @@ create table tb_producto(
 codPro	int identity(1,1) primary key not null,
 descripcionPro varchar(50) not null,
 detallePro	varchar(255) not null,
-precioPro	decimal(10,2) not null,
+precioPro	decimal(20,2) not null,
 stockPro	int not null,
 imgPro varchar(255) not null,
 codProdCat	int foreign key references tb_producto_categoria(codProdCat),
@@ -113,8 +120,11 @@ nombreTipoTarjeta varchar(20)
 )
 go
 
+select * from tb_TipoTarjeta
+
 insert into tb_TipoTarjeta values ('Visa')
 insert into tb_TipoTarjeta values ('MasterCard')
+
 
 select * from tb_TipoTarjeta
 go
@@ -132,6 +142,8 @@ lineaCredito decimal(10,2),
 creditoDisponible decimal(10,2)
 )
 go
+
+
 --Escenarios 
 -- Activado
 insert into tb_tarjeta values (1,'1234567890123451','Rodrigo Urday','111','01','2021',1,100,50)
@@ -139,8 +151,7 @@ insert into tb_tarjeta values (1,'1234567890123451','Rodrigo Urday','111','01','
 insert into tb_tarjeta values (1,'1234556789065432','Rodrigo Urday2','222','02','2022',0,100,50)
 --Saldo insuficiente
 insert into tb_tarjeta values (1,'1234556789065433','Rodrigo Urday3','333','03','2023',1,100,99)
---Tarjeta expirada	
-insert into tb_tarjeta values (1,'1234556789065434','Rodrigo Urday4','333','04','2014',1,100,99)
+
 
 select * from tb_tarjeta
  go
@@ -261,8 +272,8 @@ begin
 end
 go
 
---execute usp_eliminar_usuarios 3
---go
+execute usp_eliminar_usuarios 3
+go
 
 select * from tb_usuario
 go
@@ -274,14 +285,17 @@ begin
 	drop procedure usp_listar_tipoUsuario
 end
 go
+
 create procedure usp_listar_tipoUsuario
 as
 begin
 	select * from tb_tipo_usuario
 end
 go
+
 execute usp_listar_tipoUsuario 
 go
+
 select * from tb_tipo_usuario
 go
 */
@@ -535,10 +549,10 @@ go
  )
  as
  begin
- select tar.numeroTarjeta,tar.nombreTarjeta,tar.securityCodeTarjeta,tar.mesExpiracionTarjeta,tar.añoExpiracionTarjeta 
+ select tar.numeroTarjeta,tar.nombreTarjeta,tar.tarjetaHabilitada,tar.creditoDisponible 
  from tb_tarjeta tar where tar.idtipoTarjeta = @idTipoTarjeta 
 						
-							and tar.numeroTarjeta=@numeroTarjeta 
+						and tar.numeroTarjeta=@numeroTarjeta 
 						and tar.nombreTarjeta = @nombreTarjeta 
 						and tar.securityCodeTarjeta=@securityCodeTarjeta 
 						and tar.mesExpiracionTarjeta = @mesExpiracionTarjeta
@@ -546,253 +560,8 @@ go
  end
  go
 
- select * from tb_usuario
- go
+ exec sp_GetTarjetaByInfo 1,'1234567890123451','Rodrigo Urday','111','01','2021'
 
- 
- select* from tb_usuario
- go
- /*
- update tb_producto
- set stockPro=0
- where codPro=1
- go*/
+ use BD_PROYECTO_INTEGRADOR
 
- select * from tb_producto
- go
-
- create table tb_estado_venta(
-codEstBol  int identity(1,1) primary key not null,
-descriBol varchar(15) not null
-)
-go
-
-insert into tb_estado_venta values ('ATENDIDO')
-insert into tb_estado_venta values ('PENDIENTE')
-GO
--- PROCEDURE PARA EL LISTADO UNA BOLETA 
-if object_id('usp_Listar_Estado_Venta') is not null
-begin
-	drop procedure usp_Listar_Estado_Venta
-end
-go
-
-create procedure usp_Listar_Estado_Venta
-as
-begin
-	select * from tb_estado_venta
-end
-go
-/*
-
-execute usp_Listar_Estado_Venta
-go
-*/
--- PROCEDURE PARA OBTENER ESTADO DE VENTA
-
-if object_id('usp_obtener_estado_venta') is not null
-begin
-	drop procedure usp_obtener_estado_venta
-end
-go
-
-create procedure usp_obtener_estado_venta
-@codEstBol int
-as
-begin
-	select * from tb_estado_venta where codEstBol=@codEstBol
-end
-go
-/*
-execute usp_obtener_estado_venta 1
-go
-*/
---- tabla
-
-
- 
-create table tb_venta(
-codBol	int identity(1,1) primary key not null,
-codUsu int foreign key references tb_usuario(codUsu),
-fechaBol date default getdate() not null,
-numTarjeta varchar(16) not null,
-precioTotal money not null,
-codEstBol int foreign key references tb_estado_venta(codEstBol) not null
-)
-go
-
--- PROCEDURE PARA ACTULIZAR EL ESTADO DE LA VENTA
-if object_id('usp_actualizar_estado_Venta') is not null
-begin
-	drop procedure usp_actualizar_estado_Venta
-end
-go
-
-create procedure usp_actualizar_estado_Venta 
-@codBol int,
-@codEstBol int
-as
-begin  
-	begin 
-		update tb_venta
-		set codEstBol = @codEstBol
-		where codBol = @codBol
-	end	 
-end
-go
-/*
-select * from tb_venta
-execute usp_actualizar_estado_Venta 1,1
-go*/
-
--- PROCEDURE PARA INSERTAR UNA VENTA QUE RETORNA EL CODBOL GENERADA
-if object_id('usp_registrar_Venta') is not null
-begin
-	drop procedure usp_registrar_Venta
-end
-go
-
-create procedure usp_registrar_Venta 
-@codUsu int,
-@numTarjeta varchar(16),
-@precioTotal money,
-@codEstBol int,
-@codBol int out
-as
-begin 
-	insert into tb_venta values(@codUsu,getdate(),@numTarjeta,@precioTotal,@codEstBol)
-	 
-	set @codBol = (select top (1) codBol from tb_venta order by codBol desc)
-
-	begin
-		Declare @Monto As money
-		Set @Monto=(Select creditoDisponible From tb_tarjeta Where numeroTarjeta=@numTarjeta)
-		update tb_tarjeta
-		set creditoDisponible = @Monto - @precioTotal
-		where numeroTarjeta = @numTarjeta
-	end	 
-end
-go  
-/*
-Declare @codBol int;
-execute usp_registrar_Venta 1,'123456789',100.00,1 ,@codBol output
-print @codBol
-go*/
-
-select * from tb_venta
-
--- PROCEDURE PARA LISTAR TODAS LAS VENTAS POR EL ADMINISTRADOR
-if object_id('usp_listar_todo_ventas') is not null
-begin
-	drop procedure usp_listar_todo_ventas
-end
-go
-
-create procedure usp_listar_todo_ventas  
-as
-begin
-	select * from tb_venta
-end
-go
-
-execute usp_listar_todo_ventas 
-go
--- PROCEDURE PARA LISTAR LAS VENTAS POR EL CAMPO ESTADO
-if object_id('usp_listar_ventas_por_estado') is not null
-begin
-	drop procedure usp_listar_ventas_por_estado
-end
-go
-
-create procedure usp_listar_ventas_por_estado  
-@codEstBol int
-as
-begin
-	select * from tb_venta 
-	where codEstBol = @codEstBol
-end
-go
-
-execute usp_listar_ventas_por_estado  1
-go
--- PROCEDURE PARA LISTAR LAS VENTAS POR USUARIO
-if object_id('usp_listar_ventas_por_usuario') is not null
-begin
-	drop procedure usp_listar_ventas_por_usuario
-end
-go
-
-create procedure usp_listar_ventas_por_usuario  
-@codUsu int
-as
-begin
-	select * from tb_venta 
-	where codUsu = @codUsu
-end
-go
-/*
-execute usp_listar_ventas_por_usuario  1
-go*/
-
-
---TABLA
-
-create table tb_detalle_boleta(
-codDetBol int identity(1,1) primary key not null,
-codBol int foreign key references tb_venta(codBol) not null,
-codProd int foreign key references tb_producto(codPro) not null,
-canProd int not null,
-preProd money not null
-)
-go
-
--- PROCEDURE PARA REGISTRAR EL DETALLE DE BOLETA
-if object_id('usp_registrar_detalle_Venta') is not null
-begin
-	drop procedure usp_registrar_detalle_Venta
-end
-go
-
-create procedure usp_registrar_detalle_Venta 
-@codBol int,
-@codProd int,
-@canProd int,
-@preProd money 
-as
-begin
-	Declare @Stock As Int
-	Set @Stock=(Select stockPro From tb_producto Where codPro=@codProd)
-	begin
-		insert into tb_detalle_boleta values(@codBol,@codProd,@canProd,@preProd)
-	end
-	begin
-	update tb_producto 
-	set stockPro = @Stock - @canProd
-	where codPro = @codProd
-	end
-end
-go
-/*
-execute usp_registrar_detalle_Venta 10,1,7,10.20
-go*/
- 
-
-
--- PROCEDURE PARA LISTAR LAS DETALLES DE VENTAS POR VENTAS
-if object_id('usp_listar_detalle_ventas_por_ventas') is not null
-begin
-	drop procedure usp_listar_detalle_ventas_por_ventas
-end
-go
-
-create procedure usp_listar_detalle_ventas_por_ventas  
-@codBol int
-as
-begin
-	select * from tb_detalle_boleta
-	where codBol = @codBol
-end
-go
-/*
-execute usp_listar_detalle_ventas_por_ventas  1
-go*/
+ select * from tb_tarjeta
